bc9a14e78d7a44e712bb68f71eb57372449c07f1
LucOrt91 <103439193+LucOrt91@users.noreply.github.com>
2022-06-29 18:34:00 -0300

    agregado de boostrap, educacion, experiencia, hard y soft skills, proyectos y el footer

--- Frontend_proyecto/src/app/app.module.ts
+++ Frontend_proyecto/src/app/app.module.ts
@@ -8,7 +8,13 @@ import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { HardSoftSkillComponent } from './components/hard-soft-skill/hard-soft-skill.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,12 +24,19 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     SocialNetworksComponent,
     BannerComponent,
     AcercaDeComponent,
+    ExperienciaComponent,
+    HardSoftSkillComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
