af6be079dfb05d48c4ff109c857f2933937db68d
LucOrt91 <103439193+LucOrt91@users.noreply.github.com>
2022-06-29 19:13:29 -0300

    creacion del Backend

---
+++ Backend_proyecto/src/main/java/com/portfolio/Lucas/Ortega/LucasOrtegaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Lucas.Ortega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasOrtegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasOrtegaApplication.class, args);
+	}
+
+}
---
+++ Backend_proyecto/src/test/java/com/portfolio/Lucas/Ortega/LucasOrtegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Lucas.Ortega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasOrtegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
